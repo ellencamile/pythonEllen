4a0e4c8aeb95e2e9714308e7bcc43e1156ad62e1
Ellen Camile <ellencamile.55@gmail.com>
2021-01-27 15:52:53 -0200

    iniciando arquivo em python

---
+++ exemplo1.py
@@ -0,0 +1,4 @@
+nome = input("Qual seu nome? ")
+end = input("Qual seu endereço? ")
+idade = input("Qual sua idade? ")
+print("Ola seu nome é:",nome, "\nVoce mora no endereço: ",end, "\ne tem ",idade,"anos\n\n")
