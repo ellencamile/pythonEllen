d840c7d6408f5e3258ccee5056557e1da55b02bf
Ellen Camile <ellencamile.55@gmail.com>
2021-02-02 15:30:20 -0200

    atividades

--- Excercicios1/atividade1.py
+++
@@ -1,7 +0,0 @@
-salario = float(input("Informe o seu salario total: "))
-horas = float(input("Informe quantas horas você trabalha por dia: "))
-
-calhorasSM = ((horas * 5) * 4)
-calHoras = (salario / calhorasSM)
-
-print("Você recebe ",calhoras," por dia")
---
+++ Excercicios1/resposta1.py
@@ -0,0 +1,8 @@
+valor = int(input("Informe um valor inteiro qualquer: "))
+'''
+if valor %2 ==0:
+    print("È par")
+else:
+    print("È impar")
+    '''
+    print("é par" if valor % 2 ==0 else "È impar")
