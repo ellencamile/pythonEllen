440352447bed4195c8f7c7450162bd54d5f9e76b
Ellen Camile <ellencamile.55@gmail.com>
2021-02-04 17:05:55 -0200

    Questão 4(while)

---
+++ Excercicios while/Questão4.py
@@ -0,0 +1,7 @@
+nome = " "
+
+while len(nome) <= 15:
+    nome = input("Informe um nome: ")
+
+        print(f"O nome {nome} possui {len(nome)} caracters")
+    print("Esse nome atende aos requisitos, ate a proxima\n\n")
