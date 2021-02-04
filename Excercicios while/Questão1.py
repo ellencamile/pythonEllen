a9cab6e4e9b9651c1cc3706f4c89e0ba560837f8
Ellen Camile <ellencamile.55@gmail.com>
2021-02-04 15:21:44 -0200

    questão 1(while)

--- Excercicios while/Questão1.py
+++ Excercicios while/Questão1.py
@@ -1,3 +1,4 @@
+from os import system
 while True: 
     login = str(input("Informe o seu login: "))
     senha = str(input("Informe a sua senha: "))
@@ -7,9 +8,18 @@ while True:
         print("Informe uma senha valida!")
     else:
         print("Você esta cadastrado, bem vindo(a)")
-
         break
+system("Cls")
 
 while True:
     login2 = str(input("Informe seu login: "))
-    senha2 = str(inpunt())
+    senha2 = str(input("Informe seu login:"))
+    
+    if login2 == login:
+        print("Nome de usuario não esta disponivel, tente outro")
+    elif login2 == senha2:
+        print("Você não pode usar a mesma palavra em login e senha, pois não é seguro.")
+        print("Informe uma senha válida!")
+    else:
+        print("Você esta cadastrado, bem vinda(a)")
+        break
